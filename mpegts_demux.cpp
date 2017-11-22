c5d5d51d5a4b4c9c56b9fc08f32ec81930ba7ce4
mmoanis <mohamed_moanis@outlook.com>
2017-11-22 10:20:39 +0200

    Parse PAT Table
    Reading PAT table information. This table contains the program number and should point to PMT table. Both tables will be used to know the number of programs/streams in the TS packets to follow.

--- mpegts_demux.cpp
+++ mpegts_demux.cpp
@@ -24,6 +24,30 @@ static void print_usage(ostream& out)
 }
 ///////////////////////////////////////////////////////////////////////////////
 
+/// Programs and Streams
+map<uint16_t, FILE*> pid2Stream;
+map<uint16_t, Program> pid2Prog;
+
+bool add_pid_stream(uint16_t pid, bool v) {
+    string fn = (v? "video" : "audio");
+    FILE* F = fopen(fn.c_str(), "wb");
+    if (!F) {
+            cerr << "add_pid_stream: Failed to allocate new stream." << endl;
+        return false;
+    }
+    assert(pid2Stream.find(pid) == pid2Stream.end());
+    pid2Stream[pid] = F;
+    return true;
+}
+void close_streams() {
+    for (map<uint16_t, FILE*>::iterator i = pid2Stream.begin(), e = pid2Stream.end(); i != e; ++i)
+    {
+        assert((*i).second);
+        fclose((*i).second);
+        //*i == NULL;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
 static bool read_packet(FILE*F, uint8_t * p) {
     size_t count = 1;
     int C = fgetc(F);
@@ -55,8 +79,6 @@ static bool read_packet(FILE*F, uint8_t * p) {
     }
     return true;
 }
-FILE* V, A;
-//map<uint16_t, FILE*> pid2f;
 
 static bool decode_packet(uint8_t * p, uint8_t* endp) {
     if (!p) return false;
@@ -66,6 +88,7 @@ static bool decode_packet(uint8_t * p, uint8_t* endp) {
         return false;
     }
 
+/// Parse Header
     //const bool tei = static_cast<bool> (*p & 0x80);
     const bool pusi = static_cast<bool> (*p & 0x40);
     //const bool tp = static_cast<bool> (*p&0x20);
@@ -75,21 +98,79 @@ static bool decode_packet(uint8_t * p, uint8_t* endp) {
     //cout << "pid: " << pid << endl;
 
     p++;
-
-    if (pid == 33) { // TODO: Read all programs
-        const bool afc = (((*p&0x30)>>4) == 0x2)
+    const bool afc = (((*p&0x30)>>4) == 0x2)
             || (((*p&0x30)>>4) == 0x3);
-        //const bool payload = (((*p&0x30)>>4) == 0x1)
-        //    || (((*p&0x30)>>4) == 0x3);
-        //const uint8_t cc = *p & 0xF;
+    //const bool payload = (((*p&0x30)>>4) == 0x1)
+    //    || (((*p&0x30)>>4) == 0x3);
+    //const uint8_t cc = *p & 0xF;
 
-        p++;
-        if (afc) {
-            // escape it
-            const uint8_t afcl = *p++;
-            p += afcl;
+    p++;
+    if (afc) {
+        // escape it
+        const uint8_t afcl = *p++;
+        p += afcl;
+    }
+/// Parse Header
+
+    if (pid == MPEGTS_PID_PAT) {
+        cout << "decode_packet: Found PAT Section." << endl;
+        uint8_t pointer = *p++;
+        p += pointer; // Jump to table at pointer offset
+/// Parse Table
+        uint8_t tableID = *p++;
+        const bool ssi = static_cast<bool> (*p&0x80); // PAT/PMT/CAT == 1
+        const bool pb = static_cast<bool> (*p&0x40); // PAT/PMT/CAT == 0
+        if (((*p&0x30)>>4) != 0x03 ) {
+            cerr << "decode_packet: Table reserved bits not set." << endl;
+            return false;
+        }
+        sp = p++;
+        uint16_t slength = uint16_t((*sp&0x03)<<10) | uint16_t(*p++);
+        if (p + slength >= endp) {
+            cerr << "decode_packet: bad PAT section length." << endl;
+            return false;
         }
 
+        /// SECTION
+        sp = p++;
+        uint16_t tid = uint16_t(*sp<<8) | uint16_t(*p++);
+        if (((*p&0xC0)>>6) != 0x03) {
+            cerr << "decode_packet: Section reserved bits not set." << endl;
+            return false;
+        }
+        uint8_t version = (*p&0x3E)>>1;
+        const  bool sni = static_cast<bool> (*p++&0x1);
+
+        uint8_t sno = *p++;
+        uint8_t lsno = *p++;
+
+        /// PAT
+        uint16_t pnum = uint16_t(p[0]<<8) | uint16_t(p[1]);
+        p+=2;
+
+        if (((*p&0xE0)>>5) != 0x07) {
+            cerr << "decode_packet: PAT reserved bits not set." << endl;
+            return false;
+        }
+        uint16_t pmid = uint16_t((p[0]&0x1F)<<8) | uint16_t(p[1]);
+        p+=2;
+
+        cout << "PNUM: " << pnum << " PMAP PID: " << pmid << endl;
+        /// PAT
+
+        uint32_t crc = uint32_t(p[0]<<24) | uint32_t(p[1]<<16) | uint32_t(p[2]<<8) | uint32_t(p[3]);
+        p += 4;
+        cout << "CRC: " << hex << crc << dec << endl;
+        /// SECTION
+
+        cout << "PAT: " << int(tableID) << ' ' << slength << endl;
+        return true;
+/// Parse Table
+    }
+
+    if (pid == 33) { // TODO: Read all programs
+        if (pid2Stream.find(pid) == pid2Stream.end()) add_pid_stream(pid, true);
+        FILE* V = pid2Stream[pid];
         /// Read the payload
         if (pusi) {
             // new packet
@@ -152,7 +233,7 @@ int main(int argc, char *argv[])
     F = fopen(filename, "rb");
     if (!F) return 1;
 
-    V = fopen("video", "wb");
+    //V = fopen("video", "wb");
     //A = fopen("audio")
 
     uint8_t packet[PACKET_SIZE];
@@ -162,6 +243,8 @@ int main(int argc, char *argv[])
 
     }
 
-    fclose(V);
+    close_streams();
+    //fclose(V);
     fclose(F);
+
 }
